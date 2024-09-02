3b683d0a44100026ae22eadb664b2d54854c5c25
Keith Perkins <keith.perkins@cnu.edu>
2024-09-02 13:46:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ma1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ma1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    Button but2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        but2=findViewById(R.id.button2);
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Button 2 clicked");
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    public void doButton1(View view) {
+        tv.setText("Button 1 clicked");
+    }
+}
